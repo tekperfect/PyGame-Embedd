381ad643cdeb1bf045e50c86cfa81171161ca787
ninjaaung <ninja.aung02@gmail.com>
2020-06-25 02:09:45 -0700

    changed port

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 443))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 80))
